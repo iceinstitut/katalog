fa17bc65966087e5c83c01b6ae24327da7a8355f
iceinstitut <108342553+iceinstitut@users.noreply.github.com>
2025-09-05 20:06:03 +0700

    Update load-head.js

--- load-head.js
+++ load-head.js
@@ -1,5 +1,19 @@
 fetch("/konten/head.html")
   .then(response => response.text())
   .then(data => {
-    document.head.insertAdjacentHTML("beforeend", data);
+    const temp = document.createElement("div");
+    temp.innerHTML = data;
+
+    // Proses <link> langsung
+    temp.querySelectorAll("link").forEach(link => {
+      document.head.appendChild(link);
+    });
+
+    // Proses <script> manual
+    temp.querySelectorAll("script").forEach(oldScript => {
+      const newScript = document.createElement("script");
+      newScript.src = oldScript.src;
+      if (oldScript.defer) newScript.defer = true;
+      document.head.appendChild(newScript);
+    });
   });
